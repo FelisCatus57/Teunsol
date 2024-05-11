aeb7c827ef55bfa3fb6e68f79b18fab6db410051
computerkingminsu <ya5656@naver.com>
2024-05-11 16:45:33 +0900

    feat Navbar 스크롤감지 css변화, 배너이미지 추가

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { useTranslations } from 'next-intl';
+import Logo from '@/components/main/header/logo';
+import TransLocale from '@/components/main/header/transLocale';
+import Navbar from '@/components/main/header/navbar';
+import Image from 'next/image';
+import BackgroundImage from '@/components/main/backgroundImage';
 
 export default function Home() {
-  const t = useTranslations('Index');
+  const t = useTranslations('Header');
 
   return (
     <>
-      <div className="flex space-x-4 px-64 bg-slate-200 h-24 ">
-        <div>{t('title')}</div>
-      </div>
+      {/* 헤더 */}
+      {/* 배경 이미지 */}
+      <header className="w-screen  h-[100vh] relative">
+        {/* <Image src="/image/BackGround/메인배경1.jpg" alt="logo" layout="fill" /> */}
+        <BackgroundImage />
+        {/* 네비게이션 */}
+        <Navbar>
+          {/* 로고 */}
+          <Logo />
+          {/* 네비게이션 메뉴 */}
+          <div className="flex w-2/3 justify-between ">
+            <div>{t('Nav1')}</div>
+            <div>{t('Nav2')}</div>
+            <div>{t('Nav3')}</div>
+            <div>{t('Nav4')}</div>
+          </div>
+          {/* 언어 변경 */}
+          <TransLocale />
+        </Navbar>
+      </header>
+      <div className="h-96">asdsad</div>
     </>
   );
 }
---
+++ src/components/main/header/navbar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+export default function Navbar({ children }: { children: React.ReactNode }) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  return (
+    <>
+      <nav
+        className={`w-screen fixed top-0 left-0 right-0  flex justify-center items-center h-16 px-64 transition-all duration-300 ease-in-out  ${
+          isScrolled ? 'bg-white text-black' : 'bg-black bg-opacity-15 text-white'
+        }`}
+      >
+        {children}
+      </nav>
+    </>
+  );
+}
