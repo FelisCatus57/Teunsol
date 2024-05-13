4e058a8cd216b3ec75e596f545fdc78d12b19d06
computerkingminsu <ya5656@naver.com>
2024-05-13 17:15:52 +0900

    feat: mainpage body contents1

--- src/components/home/main/contents1/Contents1.tsx
+++ src/components/home/main/contents1/Contents1.tsx
@@ -35,9 +35,10 @@ export default function Contents1() {
     <>
       {/* 왼쪽 컨텐츠 */}
       <div className="h-[41.75rem] flex ">
-        <div className="w-1/2 h-full flex flex-col justify-center items-center relative  ">
+        <div className="w-1/2 h-full flex flex-col justify-center items-center relative pb-16 ">
           <Image src="/image/homebody/contents1/img1.jpg" alt="contents1" layout="fill" objectFit="cover" />
           <div className="absolute flex flex-col justify-center items-center w-full drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            <span className="text-5xl text-white mb-24">TeunSol</span>
             <LeftText1 />
             <div className="flex justify-between w-3/6 ">
               <div className="flex flex-col justify-center items-center ">
@@ -52,10 +53,27 @@ export default function Contents1() {
           </div>
         </div>
         {/* 오른쪽 컨텐츠 */}
-        <div className="w-1/2 h-full flex flex-col justify-center items-center">
-          <div className="w-full h-1/3 bg-slate-200">a</div>
-          <div className="w-full h-1/3 bg-red-200">a</div>
-          <div className="w-full h-1/3 bg-slate-200">a</div>
+        <div className="w-1/2 h-full flex flex-col justify-center items-center relative overflow-hidden">
+          <Image src="/image/homebody/contents1/img2.jpg" alt="contents1" layout="fill" objectFit="cover" />
+          <div className="w-full h-1/3 flex justify-center items-center text-5xl text-white">
+            <span className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Business</span>
+          </div>
+          <div
+            className={`w-full h-[45%] relative transition-all duration-700 ease-out transform  ${
+              isScrolled ? ' opacity-100 translate-y-0' : ' opacity-0  translate-y-10 '
+            }`}
+          >
+            <div className="relative w-full h-full hover:scale-110 transition-transform duration-500 ease-in-out  ">
+              <Image src="/image/homebody/contents1/img3.jpg" alt="contents1" layout="fill" objectFit="cover" />
+            </div>
+          </div>
+          <div
+            className={`w-full h-[22%] flex justify-center items-center text-3xl bg-white text-gray-600 transition-opacity duration-700 ease-out transform transition-transform ${
+              isScrolled ? 'opacity-100 translate-y-0 ' : 'opacity-0 translate-y-8 '
+            }`}
+          >
+            <span>Steel</span>
+          </div>
         </div>
       </div>
     </>
