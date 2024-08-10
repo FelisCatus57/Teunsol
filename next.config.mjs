6dff25d22cf2779fa1de12631b10c2aa2537922c
scorve12 <scorve5322@gmail.com>
2024-08-10 15:29:14 +0900

    fix 배포설정 AWS설정

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  output: 'export',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
